b675dc58f50ce4d47f8e8e54e8d3f7cdc3c20add
zven <andzven@gmail.com>
2022-06-26 17:31:14 +0800

    fix

--- lib/web3.ex
+++ lib/web3.ex
@@ -21,7 +21,7 @@ defmodule Web3 do
 
       @default_middleware [
         Web3.Middleware.Parser,
-        Web3.Middleware.RequestInspector,
+        # Web3.Middleware.RequestInspector,
         Web3.Middleware.ResponseFormatter
         # Web3.Middleware.Logger
       ]
