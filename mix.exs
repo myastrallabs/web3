efaed9c5e6194bb719cbd9ff531053f8b6232703
zven <andzven@gmail.com>
2022-06-27 23:49:17 +0800

    Initial release.

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Web3.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/zven21/web3"
+
   def project do
     [
       app: :web3,
-      version: "0.1.0",
+      version: @version,
+      description: "A Elixir library for interacting with Ethereum",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      name: "web3",
+      source_url: @github
     ]
   end
 
@@ -23,13 +31,39 @@ defmodule Web3.MixProject do
   defp deps do
     [
       {:jason, "~> 1.3"},
-      {:ecto, "~> 3.7", optional: true},
+      {:ecto, "~> 3.7"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_keccak, "~> 0.4.0"},
       {:ex_rlp, "~> 0.5.3"},
       {:curvy, "~> 0.3.0"},
       {:httpoison, "~> 1.8"},
-      {:mox, "~> 1.0", only: [:test]}
+      {:mox, "~> 1.0", only: [:test]},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "web3",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* license* CHANGELOG* changelog*),
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md", "CHANGELOG.md"],
+      maintainers: ["zven21"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "CHANGELOG.md": [],
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @github,
+      formatters: ["html"]
     ]
   end
 end
